2dc3ecc39237b378f6a5e5e1216808059e8648b8
feng.zixin@ebiz-fooge.com <FENGZIXIN250>
2023-05-08 17:57:10 +0800

    初始化

---
+++ src/main/java/threadLocalRandom/randomTest.java
@@ -0,0 +1,12 @@
+package threadLocalRandom;
+
+import java.util.Random;
+
+public class randomTest {
+    public void test01(){
+        Random random = new Random();
+        for (int i=0;i<10;i++){
+
+        }
+    }
+}
