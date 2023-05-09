1af4d3fa2cbffec162a337477575ae2dfbcdbb97
feng.zixin@ebiz-fooge.com <FENGZIXIN250>
2023-05-09 17:57:50 +0800

    初始化

--- src/main/java/threadLocalRandom/randomTest.java
+++ src/main/java/threadLocalRandom/randomTest.java
@@ -3,10 +3,14 @@ package threadLocalRandom;
 import java.util.Random;
 
 public class randomTest {
-    public void test01(){
+    public static void test01(){
         Random random = new Random();
         for (int i=0;i<10;i++){
-
+            System.out.println(random.nextInt(5));
         }
+    }
+
+    public static void main(String[] args) {
+        test01();
     }
 }
